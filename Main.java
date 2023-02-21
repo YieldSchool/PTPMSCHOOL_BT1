9ab9c668630b0f16ac249f2e60833e0a058589cf
B2014682 <nhuong4682@gmail.com>
2023-02-21 09:32:51 +0700

    Third commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    // Vòng l?p for hi?n th? các s? t? 1 d?n 10
+    for (int i = 1; i <= 10; i++) {
+      System.out.print(i + " ");
+    }
+  }
+}
